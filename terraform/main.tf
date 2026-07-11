d678d00dc7c26ac1882de6e64a328df260eae519
devanshsha911 <devanshsha911@gmail.com>
2026-07-11 09:57:17 +0530

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,64 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+resource "aws_security_group" "cassandra_sg" {
+  name = "cassandra-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9042
+    to_port     = 9042
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 7000
+    to_port     = 7000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 7199
+    to_port     = 7199
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "cassandra" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.cassandra_sg.id]
+
+  tags = {
+    Name = "Cassandra-Server"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "instance_type" {
+  default = "t3.small"
+}
+
+variable "key_name" {
+  description = "AWS Key Pair Name"
+}
